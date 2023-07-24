fbf4a566562ae9865922096e63ad0c380cc9cb3f
c6ai <110556245+c6ai@users.noreply.github.com>
2023-07-24 15:56:26 +0100

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -1,5 +1,6 @@
 ## https://docs.google.com/presentation/d/1V9VdS3YtBElMsbKKkGTcoRM5Lw-nuWjbDw4NflOZa4M/edit?usp=sharing
 ## https://github.com/c6ai/IDA/blob/main/IDA.py
+##TODO: Perform GSCSP (Gen Sim CSP (Conjugacy Search Problem)) Cryptanalysis of AE (Algebraic Eraser) Security (Gunnells 2011)
 
 # Import some libraries
 import numpy as np
